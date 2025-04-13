967209605537679f8be8a90d97e22c748bf9eefc
“唐慧” <1491966730@qq.com>
2025-04-13 23:00:58 +0800

    学JavaScript

---
+++ javascript/03_数据类型分类.js
@@ -0,0 +1,18 @@
+var num = 10;
+var str = "hello";
+var bool = true;
+
+// 对象：狭义对象：键值对集合
+// 对象：广义对象：js中一切皆对象
+var obj = {
+    name:"Tang",
+    age:18
+}
+
+
+//undefined：声明了变量，但是没有赋值 未定义
+//null：声明了变量，并且赋值为null 空对象
+var hello
+
+//初始化
+var timer = null;
